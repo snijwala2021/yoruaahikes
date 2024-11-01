5ff6a1e8ba4713b1efb751e3341335dc8e428493
snijwala2021 <138153159+snijwala2021@users.noreply.github.com>
2024-11-02 01:26:06 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Menu, Mountain, X } from 'lucide-react'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
 
